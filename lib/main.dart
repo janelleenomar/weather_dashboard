9f26a5603b0d8904994131fc2e21c3c5b1a29f56
Rose Janelle Enomar <janelleenomar@gmail.com>
2026-03-25 23:23:02 +0800

    remove 'Hello World!' text from the main app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
         ),
       ),
     );
