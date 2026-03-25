90c134e694a140a102cefede8ca324e29c51057a
Rose Janelle Enomar <janelleenomar@gmail.com>
2026-03-25 23:49:33 +0800

    refactor layout in MainApp for improved structure and styling

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFFDDE4ED),
+        backgroundColor: const Color(0xFFDDE4ED),
         body: Center(
-          child: Text(
-            'Weather Dashboard',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 80),
+              const Text(
+                'Weather Dashboard',
+                style: TextStyle(
+                  fontSize: 24, 
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 500,
+                height: 450,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFDDE4ED),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(25),
+                      spreadRadius: 10,
+                      blurRadius: 15,
+                      offset: const Offset(5, 10),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
